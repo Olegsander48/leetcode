fc107df810bc483f43463bbc95f0a92545b4220b
Aleksandr Prigodich <prigodichaleks@gmail.com>
2025-10-14 07:58:27 +0000

    Code challenge submission for question: 169 Majority Element

---
+++ 169 Majority Element/169majority-element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.merge(nums[i], 1, Integer::sum);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > nums.length / 2) {
+                return entry.getKey();
+            }
+        }
+        return -1;
+    }
+}
